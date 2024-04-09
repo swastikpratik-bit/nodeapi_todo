a9d8fb8c913865a3427dadd9d7e3a2dceae3e927
swastik pratik <swastiksingh368@gmail.com>
2024-04-09 11:15:26 +0530

    fixing some

--- data/database.js
+++ data/database.js
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 export const connectToDB = () => {
     mongoose
         .connect(process.env.MONGO)
-        .then(() => console.log("Database Connected"))
+        .then((it) => console.log(`Database Connected with HOST : ${it.connection.host}`))
         .catch((e) => console.log(e));
 };
